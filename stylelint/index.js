c0ce7b424b17bbec8947b178fc11a3e82fcde759
César Guadarrama <cesar@ocho.co>
2022-10-31 15:04:42 -0600

    Update rules

--- stylelint/index.js
+++ stylelint/index.js
@@ -3,7 +3,15 @@
 module.exports = {
   processors: ['stylelint-processor-styled-components'],
   extends: [
-    'stylelint-config-recommended',
+    'stylelint-config-standard-scss',
     'stylelint-config-styled-components',
   ],
+  customSyntax: 'postcss-scss',
+  rules: {
+    'custom-property-pattern': null,
+    'value-keyword-case': null,
+    'keyframes-name-pattern': null,
+    'string-quotes': 'single',
+    'color-hex-case': 'upper',
+  },
 }
