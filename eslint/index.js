20fcbfbe6a948bfc30290f430a1dc4c975b80f15
César Guadarrama <cesar@ocho.co>
2023-08-10 10:36:48 -0600

    Update rules

--- eslint/index.js
+++ eslint/index.js
@@ -3,41 +3,47 @@
 const MAX_PARAMS = 3
 
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jsx-a11y', 'import', 'react-hooks', 'react'],
+  ignorePatterns: 'src/generated/**/*',
+  plugins: [
+    // General
+    '@typescript-eslint',
+    // React
+    'react-hooks',
+    'react',
+    // Accessibility
+    'jsx-a11y',
+    // Sort
+    'simple-import-sort',
+    // Other
+    'sort-keys-fix',
+  ],
   env: {
     es6: true,
     node: true,
     jest: true,
     browser: true,
   },
-  ignorePatterns: 'src/generated/**/*',
   extends: [
-    // Recommended general
+    // General
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
 
-    // Prettier-compatible
+    // Prettier
     'prettier',
 
-    // React rules
+    // React
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
 
     // Accessibility
     'plugin:jsx-a11y/strict',
-
-    // Imports
-    'plugin:import/recommended',
-    'plugin:import/typescript',
   ],
   rules: {
-    // TypeScript
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/no-redeclare': [1],
-
-    // ESLint
+    /*
+      G E N E R A L
+    */
     'arrow-body-style': ['warn', 'as-needed'],
     'max-lines': ['warn', { max: 500, skipBlankLines: true }],
     'max-params': ['warn', MAX_PARAMS],
@@ -58,7 +64,16 @@ module.exports = {
     ],
     'func-style': ['warn', 'declaration', { allowArrowFunctions: true }],
 
-    // React
+    /*
+      T Y P E S C R I P T
+    */
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '^_' }],
+    '@typecript-eslint/no-redeclare': [1],
+
+    /*
+      R E A C T
+    */
     'react/react-in-jsx-scope': 'off',
     'react/jsx-boolean-value': ['warn', 'never'],
     'react/prop-types': [0],
@@ -66,37 +81,47 @@ module.exports = {
       'warn',
       {
         shorthandLast: true,
+        multiline: 'first',
+        ignoreCase: true,
         reservedFirst: true,
-        noSortAlphabetically: true,
+        locale: 'auto',
       },
     ],
     'react/jsx-no-useless-fragment': 'warn',
 
-    // Import
-    'import/order': [
+    /*
+      I M P O R T S
+    */
+    'simple-import-sort/imports': [
       'warn',
       {
         groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
+          // Packages `node` or `react` related packages come first.
+          ['^node:', '^react', '^@?\\w'],
+          // Internal packages.
+          ['^(@|components)(/.*|$)'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.?(css)$'],
         ],
-        'newlines-between': 'always-and-inside-groups',
       },
     ],
-    'import/exports-last': 'warn',
+
+    /*
+      O T H E R
+    */
+    'sort-keys-fix/sort-keys-fix': [
+      'warn',
+      'asc',
+      { caseSensitive: true, natural: false },
+    ],
   },
   settings: {
     react: { version: 'detect' },
-    'import/resolver': {
-      typescript: true,
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        moduleDirectory: ['node_modules', 'src'],
-      },
-    },
   },
 }
